3ea9c8e6fc64aeebc40aa186de1e2eb4857b2d49
liugang <liugang@ufenqi.com>
2015-06-28 00:50:25 +0800

    add database

--- routes/database.js
+++ routes/database.js
@@ -11,57 +11,69 @@ var memberSchema = mongo.Schema(
 var memberTable = db.model('badminton', memberSchema);
 
 var memberOperators = {};
-//name, identity, date are required, valid is optional
+
+/*
+* insert a record
+* name, identity, date are required, valid is optional
+*/
 memberOperators.insert = function(data, callback){
 	if(!data || !data.name || !data.identity || !data.date){
 		throw '用户名、用户标识和活动日期不能为粉';
 	}
-	if(!data.valid){
+	if(data.valid === null || data.valid == undefined){
 		data.valid = true;
 	}
 	var newMember = new memberTable(data);
      newMember.save(callback);
 };
 
-memberOperators.get = function(identity, callback){
-	if(!id){
+/*
+* get apply status of member, by using the member id or/and active date
+*
+*/
+memberOperators.get = function(data, callback){
+	if(!data || !data.identity){
 		throw '用户标识不能为空';
 	}
-	memberTable.findOne({identity : identity}, callback);
+	memberTable.findOne({identity : data.identity}, callback);
 }
 
-//identity is required
+/*
+* update member status
+* identity is required
+*
+*/
 memberOperators.update = function(data, callback){
-	if(!data.identity){
+	if(!data || !data.identity){
 		throw '用户标识不能为空';
 	}
 	memberTable.update({identity : data.identity}, data, {}, callback );
 }
 
 //list all data by specifing date
-memberOperators.list = function(date, callback){
-	memberTable.find({date : date}, callback);
+memberOperators.list = function(data, callback){
+	memberTable.find(data, callback);
 }
 
 module.exports = memberOperators;
 
 //test insert
-// memberOperators.insert('刘刚', 'liugang', true, '20150626', function(err){
+// memberOperators.insert({name: '刘刚', identity: 'liugang', valid: true, date : '20150626'}, function(err){
 // 	err?console.log("报名失败："+err):console.log("报名成功");
 // })
 
 //test get
-// memberOperators.get('liugang', function(err, docs){
+// memberOperators.get({identity: 'liugang'}, function(err, docs){
 // 	err?console.log('查询失败：'+err):console.log(docs);
 // });
 
 //test update
-// memberOperators.update('liugang', 'ttt', '20150606', 'false', function(err, doc){
+// memberOperators.update({identity:'liugang', name:'ttt', date:'20150606', valid:false}, function(err, doc){
 // 	err?console.log('更新失败：'+err):console.log(doc);
 // })
 
 //test list
-// memberOperators.list(function(err, docs){
+// memberOperators.list({date: '20150606'}, function(err, docs){
 // 	err?console.log('列举失败：'+err):console.log(docs);
 // });
 
